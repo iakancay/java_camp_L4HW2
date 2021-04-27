4d3164d47bfd6a00076ed610b58290bf1ecc84dc
iakancay <iakancay@gmail.com>
2021-04-28 00:10:09 +0300

    kodlamaio class uygulamasi

---
+++ homework1/src/homework1/Course.java
@@ -0,0 +1,16 @@
+package homework1;
+
+public class Course {
+	public Course(int id,String name,String lecturer,int progress) {
+		this.id=id;
+		this.name=name;
+		this.lecturer=lecturer;
+		this.progress=progress;
+	}
+	
+	int id;
+	String name;
+	String lecturer;
+	int progress;
+
+}
