eefb3e5edb92305aeaa75a41cb825a8ff9bcc701
iakancay <77550297+iakancay@users.noreply.github.com>
2021-05-04 17:03:12 +0300

    Delete homework2 directory

--- homework2/src/homework2/InstructorManager.java
+++
@@ -1,8 +0,0 @@
-package homework2;
-
-public class InstructorManager extends UserManager{
-	public void add(Instructor instructor) {
-		System.out.println(instructor.getFirstName()+" "+instructor.getLastName()+" was appointed to this course as a instructor..");
-	}
-
-}
--- homework2/src/homework2/UserManager.java
+++
@@ -1,8 +0,0 @@
-package homework2;
-
-public class UserManager {
-	public void add(User user) {
-		System.out.println(user.getFirstName()+" "+user.getLastName()+" was added to course successfully.");
-	}
-
-}
